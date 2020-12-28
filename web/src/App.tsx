441834098d0ae95d21118a63fe35d5558d17478e
Marcel Ebert <mail@marcel-ebert.de>
2020-12-28 15:36:59 +0100

    Add @material-ui dependency

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,9 @@
+import CssBaseline from "@material-ui/core/CssBaseline"
+
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <header className="App-header">
         <p>Test</p>
       </header>
