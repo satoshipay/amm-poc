2fcec37aa2b5b37c31fc49fcf6f17d91c0270649
Marcel Ebert <mail@marcel-ebert.de>
2021-02-10 16:34:36 +0100

    Add more env vars to config

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,8 +1,17 @@
+import { fail } from "assert"
+import { parseAssetID } from "./lib/stellar"
+
 const config = {
   marketMakerAccountIdMainnet: process.env.REACT_APP_MARKET_MAKER_ACCOUNT_ID_MAINNET,
   marketMakerAccountIdTestnet:
     process.env.REACT_APP_MARKET_MAKER_ACCOUNT_ID_TESTNET || "GCDXHFRLT3NRSCYGONQ3UUT7IZOG4LW2M4SKFOYKVIZHEWH76BEBJSXB",
   tssURL: process.env.REACT_APP_TSS_URL || "http://localhost:3001",
+  assetOne: parseAssetID(process.env.REACT_APP_ASSET_ONE || fail("REACT_APP_ASSET_ONE not set")),
+  assetTwo: parseAssetID(process.env.REACT_APP_ASSET_TWO || fail("REACT_APP_ASSET_TWO not set")),
+  liquidityProviderAsset: parseAssetID(process.env.REACT_APP_ASSET_LP || fail("REACT_APP_ASSET_LP not set")),
+  swapFeePercent: process.env.REACT_APP_SWAP_FEE_PERCENT
+    ? Number.parseFloat(process.env.REACT_APP_SWAP_FEE_PERCENT)
+    : fail("process.env.REACT_APP_SWAP_FEE_PERCENT not set"),
 }
 
 export default config
